edc66fad3ff3f0363f6906230cd971b2add21966
Mogreyn <alv87@bk.ru>
2024-12-14 18:41:53 +0200

    Add today's changes (App.js and Chart.sass)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import MoviePage from "./components/MoviePage/MoviePage";
 import Chart from "./components/Chart/Chart"; // Теперь Chart будет главной страницей
-// import About from "./components/About/About"; // Страница "О нас"
+import About from "./components/About/About"; // Страница "О нас"
 import "./App.sass"; // Главные стили
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Chart />} /> {/* Главная страница теперь - Chart */}
           <Route path="/movies/:id" element={<MoviePage />} />
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/about" element={<About />} />
           {/* Здесь можно добавить другие маршруты в будущем */}
         </Routes>
       </div>
